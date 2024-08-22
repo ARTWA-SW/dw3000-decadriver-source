d3b2da1f3468f20156cf26ecf9d3e138e03c7357
hig4342 <hig4342@naver.com>
2024-08-22 10:16:29 +0900

    :sparkles: feat: add arduino-pico driver
    Add to support arduino-pico platform and add platformio settings

---
+++ platform/arduino-pico/deca_port.cpp
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+#include <FreeRTOS.h>
+#include <task.h>
+#include <semphr.h>
+
+#include "deca_device_api.h"
+#include "dw3000_hw.h"
+#include "dw3000_spi.h"
+
+/* This file implements the functions required by decadriver */
+
+void wakeup_device_with_io(void)
+{
+  /* this would only be used if you use the dwt_wakeup_ic() API, but it's
+   * easier to directly call our own dw3000_hw_wakeup() function */
+  dw3000_hw_wakeup();
+}
+
+decaIrqStatus_t decamutexon(void)
+{
+  portENTER_CRITICAL();
+  return 0;
+}
+
+void decamutexoff(decaIrqStatus_t s) { portEXIT_CRITICAL(); }
+
+void deca_sleep(unsigned int time_ms) { vTaskDelay(pdMS_TO_TICKS(time_ms)); }
+
+void deca_usleep(unsigned long time_us) { sleep_us(time_us); }
+
+int readfromspi(uint16_t headerLength, uint8_t *headerBuffer,
+                uint16_t readLength, uint8_t *readBuffer)
+{
+  return dw3000_spi_read(headerLength, headerBuffer, readLength, readBuffer);
+}
+
+int writetospi(uint16_t headerLength, const uint8_t *headerBuffer,
+               uint16_t bodyLength, const uint8_t *bodyBuffer)
+{
+  return dw3000_spi_write(headerLength, headerBuffer, bodyLength,
+                          bodyBuffer);
+}
+
+int writetospiwithcrc(uint16_t headerLength, const uint8_t *headerBuffer,
+                      uint16_t bodyLength, const uint8_t *bodyBuffer,
+                      uint8_t crc8)
+{
+  return dw3000_spi_write_crc(headerLength, headerBuffer, bodyLength,
+                              bodyBuffer, crc8);
+}
---
+++ platform/arduino-pico/dw3000_hw.cpp
@@ -0,0 +1,161 @@
+#include <Arduino.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
+#include "deca_device_api.h"
+#include "dw3000_hw.h"
+#include "dw3000_spi.h"
+// #include "log.h"
+
+static const char *LOG_TAG = "DW3000";
+static const struct dw3000_hw_cfg *dw_hw_cfg;
+
+int dw3000_hw_init(const struct dw3000_hw_cfg *cfg)
+{
+  // LOG_INF("HW Init (RESET:%d WAKEUP:%d IRQ:%d)", cfg->reset_pin, cfg->wakeup_pin, cfg->irq_pin);
+
+  dw_hw_cfg = cfg;
+
+  /*
+   * RESET: output low, open drain, no pull-up
+   * normally used as input to see when DW3000 is ready
+   */
+  if (cfg->reset_pin != -1)
+  {
+    pinMode(cfg->reset_pin, INPUT);
+
+    /* check reset state */
+    int timeout = 1000;
+    while (digitalRead(cfg->reset_pin) == LOW && --timeout > 0)
+    {
+      vTaskDelay(1);
+    }
+    if (timeout <= 0)
+    {
+      return DWT_ERROR;
+    }
+  }
+
+  /*
+   * WAKEUP: output high
+   */
+  if (cfg->wakeup_pin != -1)
+  {
+    pinMode(cfg->wakeup_pin, OUTPUT);
+    digitalWrite(cfg->wakeup_pin, LOW);
+  }
+
+  return dw3000_spi_init(cfg);
+}
+
+int dw3000_hw_reinit(void) { return dw3000_hw_init(dw_hw_cfg); }
+
+static void dw3000_isr(void *args)
+{
+  while (digitalRead(dw_hw_cfg->irq_pin))
+  {
+    dwt_isr();
+  }
+}
+
+int dw3000_hw_init_interrupt(void)
+{
+  if (dw_hw_cfg->irq_pin == -1)
+  {
+    // LOG_ERR("IRQ pin is not defined");
+    return DWT_ERROR;
+  }
+
+  pinMode(dw_hw_cfg->irq_pin, INPUT);
+  attachInterruptParam(digitalPinToInterrupt(dw_hw_cfg->irq_pin), dw3000_isr, CHANGE, nullptr);
+
+  return DWT_SUCCESS;
+}
+
+void dw3000_hw_interrupt_enable(void)
+{
+  if (dw_hw_cfg->irq_pin != -1)
+  {
+    irq_set_enabled(dw_hw_cfg->irq_pin, true);
+  }
+}
+
+void dw3000_hw_interrupt_disable(void)
+{
+  if (dw_hw_cfg->irq_pin != -1)
+  {
+    irq_set_enabled(dw_hw_cfg->irq_pin, false);
+  }
+}
+
+bool dw3000_hw_interrupt_is_enabled(void)
+{
+  if (dw_hw_cfg->irq_pin != -1)
+  {
+    return irq_is_enabled(dw_hw_cfg->irq_pin);
+  }
+  else
+  {
+    return false;
+  }
+}
+
+void dw3000_hw_fini(void)
+{
+  // LOG_INF("HW fini");
+
+  if (dw_hw_cfg->irq_pin != -1)
+  {
+    detachInterrupt(dw_hw_cfg->irq_pin);
+  }
+
+  dw3000_spi_fini();
+}
+
+void dw3000_hw_reset(void)
+{
+  if (dw_hw_cfg->reset_pin == -1)
+  {
+    // LOG_ERR("Reset pin is not defined");
+    return;
+  }
+
+  // LOG_INF("HW reset");
+  pinMode(dw_hw_cfg->reset_pin, OUTPUT);
+  digitalWrite(dw_hw_cfg->reset_pin, LOW);
+  vTaskDelay(1);
+  digitalWrite(dw_hw_cfg->reset_pin, HIGH);
+  vTaskDelay(2);
+  pinMode(dw_hw_cfg->reset_pin, INPUT);
+}
+
+/** wakeup either using the WAKEUP pin or SPI CS */
+void dw3000_hw_wakeup(void)
+{
+  if (dw_hw_cfg->wakeup_pin != -1)
+  {
+    /* Use WAKEUP pin if available */
+    // LOG_INF("WAKEUP PIN");
+    digitalWrite(dw_hw_cfg->wakeup_pin, HIGH);
+    vTaskDelay(1); // 500 usec
+    digitalWrite(dw_hw_cfg->wakeup_pin, LOW);
+  }
+  else
+  {
+    /* Use SPI CS pin */
+    // LOG_INF("WAKEUP CS");
+    digitalWrite(dw_hw_cfg->spi_cs_pin, LOW);
+    vTaskDelay(1); // 500 usec
+    digitalWrite(dw_hw_cfg->spi_cs_pin, HIGH);
+  }
+  vTaskDelay(1);
+}
+
+/** set WAKEUP pin low if available */
+void dw3000_hw_wakeup_pin_low(void)
+{
+  if (dw_hw_cfg->wakeup_pin != -1)
+  {
+    digitalWrite(dw_hw_cfg->wakeup_pin, LOW);
+  }
+}
